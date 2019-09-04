4008855c40700b9499dcf337094f5609b5b3dc88
chrisLoPresti <lopresti.chris@yahoo.com>
2019-09-04 17:42:16 -0400

    adding prod key

---
+++ server/client/build/precache-manifest.407800ffa87605bf5c0353bf3dc77d12.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe9fac4d3e37bc415b08abd25ec4a95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e1bb0e00c9744d9f5cf",
+    "url": "/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "81c0df851b3ed731fdae",
+    "url": "/static/css/main.821a7760.chunk.css"
+  },
+  {
+    "revision": "3e1bb0e00c9744d9f5cf",
+    "url": "/static/js/2.ae0a4d84.chunk.js"
+  },
+  {
+    "revision": "81c0df851b3ed731fdae",
+    "url": "/static/js/main.db2a5837.chunk.js"
+  },
+  {
+    "revision": "cf4fc05bf927b1861733",
+    "url": "/static/js/runtime~main.5ce510ce.js"
+  },
+  {
+    "revision": "6e5393a64ca23c455e8568d409e82da6",
+    "url": "/static/media/Pokemon_Hollow.6e5393a6.ttf"
+  },
+  {
+    "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
+    "url": "/static/media/Pokemon_Solid.36d26dbc.ttf"
+  },
+  {
+    "revision": "b3d23c0c6a547dc60457a0b2645fe38f",
+    "url": "/static/media/bug.b3d23c0c.png"
+  },
+  {
+    "revision": "43fad52b2a95e44a04603dd22d3bed9a",
+    "url": "/static/media/dark.43fad52b.png"
+  },
+  {
+    "revision": "3c95d0516b694173d6c4e81debf734f2",
+    "url": "/static/media/dragon.3c95d051.png"
+  },
+  {
+    "revision": "a640e22236e689553b19d1e2d4ae4d2e",
+    "url": "/static/media/electric.a640e222.png"
+  },
+  {
+    "revision": "33ca89dce2e8b91a3cd034a34fd7eea1",
+    "url": "/static/media/evolution.33ca89dc.svg"
+  },
+  {
+    "revision": "d882ac5f21557cacb47c6c159f9bbafa",
+    "url": "/static/media/fairy.d882ac5f.png"
+  },
+  {
+    "revision": "94e1c66095ad8f66ac0a8fec174bf13b",
+    "url": "/static/media/fighting.94e1c660.png"
+  },
+  {
+    "revision": "eac89c9ab48cea86b79df8e1b82a264f",
+    "url": "/static/media/fire.eac89c9a.png"
+  },
+  {
+    "revision": "31e9a60bf9b525fb01a52ba5327df4aa",
+    "url": "/static/media/flying.31e9a60b.png"
+  },
+  {
+    "revision": "c3242dce17f9635ea8a1bdc4e941b6ab",
+    "url": "/static/media/ghost.c3242dce.png"
+  },
+  {
+    "revision": "2b9b1bfc16d8b3170a9e04a5630c7ce0",
+    "url": "/static/media/grass.2b9b1bfc.png"
+  },
+  {
+    "revision": "f3a61fdc6206dba2c388d62dffffb540",
+    "url": "/static/media/ground.f3a61fdc.png"
+  },
+  {
+    "revision": "d03edb7e031f8e27874aa087b6aa34e2",
+    "url": "/static/media/ice.d03edb7e.png"
+  },
+  {
+    "revision": "5fb64b5f9c69fb4a6dca2f2788dadf81",
+    "url": "/static/media/legendary.5fb64b5f.svg"
+  },
+  {
+    "revision": "547fccf5b27765539d8d7a7bbc044945",
+    "url": "/static/media/loadingPage.547fccf5.gif"
+  },
+  {
+    "revision": "3ddca6f0a8a026039cfc388b12912338",
+    "url": "/static/media/megaSymbol.3ddca6f0.png"
+  },
+  {
+    "revision": "8861201f1a35ef9bd43f42c4bc775925",
+    "url": "/static/media/mythic.8861201f.png"
+  },
+  {
+    "revision": "b03ddb6c6da7dd1f330fc11ae636b17a",
+    "url": "/static/media/no-results.b03ddb6c.png"
+  },
+  {
+    "revision": "47d2e7b2ba5c5fb00fa075be9639cc21",
+    "url": "/static/media/normal.47d2e7b2.png"
+  },
+  {
+    "revision": "b3e8f39ad4aeb9bb2e20fc08b9cb6875",
+    "url": "/static/media/poison.b3e8f39a.png"
+  },
+  {
+    "revision": "9b75b10126e024210d628ca96c4d177c",
+    "url": "/static/media/pokeball.9b75b101.png"
+  },
+  {
+    "revision": "9f235d0a9d5688481812d6a8fa95d420",
+    "url": "/static/media/pseudo.9f235d0a.png"
+  },
+  {
+    "revision": "f4486dcb1fabd0432bbf7753b0d20344",
+    "url": "/static/media/psychic.f4486dcb.png"
+  },
+  {
+    "revision": "06fcf6beaa5c7d946f57e83ce4a99843",
+    "url": "/static/media/region.06fcf6be.png"
+  },
+  {
+    "revision": "c6a84bf6791e5d43080473d604144e59",
+    "url": "/static/media/rock.c6a84bf6.png"
+  },
+  {
+    "revision": "00b3109a5a68c54f67f9afacc4319445",
+    "url": "/static/media/steel.00b3109a.png"
+  },
+  {
+    "revision": "c5d12b67cf2323c618d0b857157c3235",
+    "url": "/static/media/typesIcon.c5d12b67.png"
+  },
+  {
+    "revision": "e43fbc983ba9b7e061515d4024173a19",
+    "url": "/static/media/water.e43fbc98.png"
+  }
+]);
