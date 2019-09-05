03ce92996d3682b10ee2bac7151493b67ca59027
chrisLoPresti <lopresti.chris@yahoo.com>
2019-09-04 23:31:45 -0400

    updating git ignore

--- config/keys_dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  //key to connect to mongoDB
-  mongoURI:
-    "mongodb://clopresti21:Rocketman2!@ds311968.mlab.com:11968/team_showcase",
-  //secret or key for json web token
-  secretOrKey: "p0k3m0n2ol9"
-};
