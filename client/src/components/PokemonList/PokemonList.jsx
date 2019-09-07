003adaa785eba4269fda602f56f5312d8c4709be
chrisLoPresti <lopresti.chris@yahoo.com>
2019-09-06 21:59:41 -0400

    more fixes and date caching fix

--- client/src/components/PokemonList/PokemonList.jsx
+++ client/src/components/PokemonList/PokemonList.jsx
@@ -46,7 +46,7 @@ const PokemonList = React.memo(
       })
     });
     const isActive = canDropPokemon && isOver;
-    let backgroundColor = "dimgrey";
+    let backgroundColor = "";
     if (isActive) {
       backgroundColor = "#e53935";
     } else if (canDropPokemon) {
@@ -118,7 +118,8 @@ const PokemonList = React.memo(
                   outline: "none",
                   boxShadow:
                     "0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)",
-                  backgroundColor
+                  backgroundColor,
+                  borderRadius: 10
                 }}
                 height={height}
                 rowCount={rowCount}
