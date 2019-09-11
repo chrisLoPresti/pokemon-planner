94826743f9a57704af398162237e3ff7e4639e72
chrisLoPresti <lopresti.chris@yahoo.com>
2019-09-11 02:07:01 -0400

    changing proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,10 @@
 const proxy = require('http-proxy-middleware');
 
+const target =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5000/'
+    : 'http://pokemon-team-planner-ani.herokuapp.com/';
+
 module.exports = app => {
   app.use(
     proxy('/api', {
@@ -9,7 +14,7 @@ module.exports = app => {
   );
   app.use(
     proxy('/socket.io', {
-      target: 'http://localhost:5000/',
+      target,
       changeOrigin: true,
       ws: true
     })
